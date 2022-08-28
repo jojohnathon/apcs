568f32ee5914d3974aada6cf95754ca4bd64fa39
jojohnathon <pixelizedjohnny@gmail.com>
2022-08-27 21:47:53 -0700

    first commit

---
+++ HelloWorld Project/src/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ * Print a message to the console
+ * @author Johnathon Zheng
+ * @version Start date: August 18, 2022
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World! From Johnathon");
+
+	}
+
+}
---
+++ test/src/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package test;
+/*
+ * Prints hello world
+ * @Author: Johnathon Zheng
+ * @Date: 8/19/22
+ */
+
+
+public class HelloWorld {
+	public static void Main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
