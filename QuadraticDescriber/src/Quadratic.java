5d416bd40bd6eca7521a56a733a3ec436a0019df
jojohnathon <pixelizedjohnny@gmail.com>
2022-09-23 14:54:55 -0700

    added needed methods to quadratic and started quadraticClient

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -3,9 +3,86 @@
  * @version September 23, 2022
  * methods required to analyze a quadratic function 
  */
+import java.util.*;
+
 public class Quadratic {
     public static void quadrDescriber (double a, double b, double c) {
-        
+        System.out.println("Description of the graph of: ");
+        System.out.println("y = " + a + "x^2" + " + " + b + "x" + " + " + c);
+
+        if (a > 0.0) {
+            System.out.println("Opens: Up");
+        } else {
+            System.out.println("Opens: Down");
+        }
+        System.out.print("Axis of Symmetry: ");
+        System.out.println("Vertex: ");
+        System.out.println("x-intercept(s): ");
+        System.out.println("y-intercept: ");
     }
 
+    public static String quadForm(int a, int b, int c) {
+        if (discriminant(a, b, c) > 0) { //two solutions
+            double solutionOne = ((-1 * b) + (sqrt(discriminant(a, b, c)))) / (2 * a);
+            double solutionTwo = ((-1 * b) - (sqrt(discriminant(a, b, c)))) / (2 * a);
+            return round2(minDouble(solutionOne, solutionTwo)) + " and " + round2(max(solutionTwo, solutionOne));
+        } else if (discriminant(a, b, c) == 0) { //one solution
+            double solutionOne = ((-1 * b) + (sqrt(discriminant(a, b, c)))) / (2 * a);
+            return round2(solutionOne) + "";
+        } else { // no real solutions
+            return "no real roots";
+        }
+     }
+
+     public static double discriminant(double a, double b, double c) {
+		double answer = 0;
+		answer = (b*b) - (4 * a * c);
+		return answer;
+	}
+
+    public static double sqrt(double n) {
+		if (n < 0) throw new IllegalArgumentException("Please enter a number greater than 0");
+		double initialGuess = n / 2;
+		double answer;
+		do {
+			answer = initialGuess - ((initialGuess * initialGuess) - n) / (2 * initialGuess);
+			initialGuess = answer;
+		} while (absValue(answer * answer - n) >= 0.005);
+		return round2(answer);
+	 }
+
+     public static double round2(double num) {
+        if (num > 0) {
+            double answer = (num * 100) + 0.5; //convert to int
+            int answer1 = (int)answer; //truncate 
+            double answer2 = (double)answer1/100; //convert back to double
+            return answer2;
+        } else {
+            double answer = (num * 100) - 0.5; //convert to int
+            int answer1 = (int)answer; //truncate 
+            double answer2 = (double)answer1/100; //convert back to double
+            return answer2;
+        }
+    }
+
+    public static double absValue(double num) {
+        double answer = num;
+        if (num < 0) {
+            answer /= -1;
+        } return answer;
+    }
+
+    public static double minDouble(double num1, double num2) {
+		double answer = num1;
+		if (num1 > num2) {
+			answer = num2;
+		} return answer;
+	 }
+
+     public static double max(double num1, double num2) {
+        double answer = num1;
+        if (num1 < num2) {
+            answer = num2;
+        } return answer;
+    }
 }
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -10,20 +10,30 @@ public class QuadraticClient {
 	public static void main(String[] args) {
 		//greeting
 		System.out.println("Welcome to the Quadratic Describer");
-		System.out.println("Provide values for coefficients a, b, and");
+		System.out.println("Provide values for coefficients a, b, and\n");
 		//loop(while)
 		boolean run = true;
 		while(run) {
-			Scanner console = new Scanner(System.in);
-			int a = console.nextInt();
-			int b = console.nextInt();
-			int c = console.nextInt();
+			//get input from user
+			Scanner sc = new Scanner(System.in);
+			System.out.println("a: ");
+			double a = sc.nextDouble();
+			System.out.println("b: ");
+			double b = sc.nextDouble();
+			System.out.println("c: ");
+			double c = sc.nextDouble();
+
+			//call quadradescriber to analyze the function
+			Quadratic.quadrDescriber(a, b, c);
 
 		}
-			//get input from user
-			//call quadradescriber to analyze the function
 			//print the results
 			//ask go again?
+			System.out.println("Do you want to keep going? (type \"quit\" to end)");
+			String keepGoing = sc.next();
+			if (keepGoing.equals("quit")) {
+
+			}
 			//get answer
 		}
 
