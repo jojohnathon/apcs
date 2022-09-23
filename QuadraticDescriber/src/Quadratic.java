97353c369f379caffc21f51def79ac8b7e7ff237
jojohnathon <pixelizedjohnny@gmail.com>
2022-09-23 12:43:05 -0700

    start qd

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -1,4 +1,8 @@
-
+/*
+ * @author Johnathon Zheng
+ * @version September 23, 2022
+ * methods required to analyze a quadratic function 
+ */
 public class Quadratic {
     public static void quadrDescriber (double a, double b, double c) {
         
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -1,9 +1,30 @@
+/*
+ * @author Johnathon Zheng
+ * @version September 23, 2022
+ * Client code for calculate part 5
+ */
+import java.util.*;
 
 public class QuadraticClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//greeting
+		System.out.println("Welcome to the Quadratic Describer");
+		System.out.println("Provide values for coefficients a, b, and");
+		//loop(while)
+		boolean run = true;
+		while(run) {
+			Scanner console = new Scanner(System.in);
+			int a = console.nextInt();
+			int b = console.nextInt();
+			int c = console.nextInt();
 
-	}
+		}
+			//get input from user
+			//call quadradescriber to analyze the function
+			//print the results
+			//ask go again?
+			//get answer
+		}
 
 }
