ce182a190318b22f5a46f66271642f34e53eb0c5
jojohnathon <pixelizedjohnny@gmail.com>
2022-10-19 14:36:38 -0700

    finsih country data 2

--- Country Data Project/src/CountryClient.java
+++ Country Data Project/src/CountryClient.java
@@ -10,27 +10,55 @@ import java.io.FileNotFoundException;
 public class CountryClient {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		File inputFile = new File("Country Data Project\\src\\singleCountryData.txt");
+		
+		File inputFile = new File("Country Data Project\\src\\multipleCountry2.txt");
 		Scanner sc = new Scanner(inputFile);
 		
 		String series = sc.nextLine();
-		System.out.println(series);
+		int numYears = sc.nextInt();
+		// System.out.println(series);
 		//TODO String input; 
 		// Integer.parseInt(string) 
-		int numYears = sc.nextInt();
-		for(int i = 0; i < numYears; i++) {
-			int year = sc.nextInt();
-			System.out.print(year + "	");
-		}
 
-		String country = sc.next();
-		System.out.println();
-		for (int i = 0; i < numYears; i++) {
-			double data = sc.nextDouble();
-			System.out.print(data + "	");
-		}
 
-		System.out.println("\nThis is the " + series + " for " + country);
+		while (true) {
+			String country = sc.nextLine();
+			country = sc.nextLine();
+			if (country.equals("END")) {
+				break;
+			 }
+
+			for(int i = 0; i < numYears; i++) {
+				String year = sc.next();
+				System.out.print(year + "	");
+			}
+			double max = 0;
+			double min = 0;
+			System.out.println();
+			for (int i = 0; i < numYears; i++) {
+				double data = Math.round(sc.nextDouble() * 100.0) / 100.0;
+				System.out.print(data + "	");
+				if (i == 0) {
+					max = data;
+					min = data;
+				}
+				if (data > max) {
+					max = data;
+				} else if (data < min) {
+					min = data;
+				}
+			}
+			
+			System.out.println("\nThis is the \"" + series + "\" for " + country);
+			
+			//TODO min max
+			System.out.println("Minimum: " + min);
+			System.out.println("Maximum: " + max);
+			System.out.println();
+			
+
+		}
+		
 		sc.close();
 	}
 
