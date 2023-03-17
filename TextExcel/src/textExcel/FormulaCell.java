d15b35751ae942e22f32d202161027b9bb4a8fd6
jojohnathon <pixelizedjohnny@gmail.com>
2023-03-16 19:46:41 -0700

    finish chckpt5

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -15,11 +15,31 @@ public class FormulaCell extends RealCell {
         String[] formula = this.formula.substring(2, this.formula.length() - 2).split(" ");
         double ans = 0;
         
-        if (formula[0].equalsIgnoreCase("SUM")) {
+        if (formula[0].equalsIgnoreCase("SUM") || formula[0].equalsIgnoreCase("AVG")) {
+            String[] cellSelection = formula[1].split("-");
+            SpreadsheetLocation startingCell = new SpreadsheetLocation(cellSelection[0]);
+            SpreadsheetLocation lastCell = new SpreadsheetLocation(cellSelection[1]);
+            int iteratedCellCount = 0; 
             
-        } else if (formula[0].equalsIgnoreCase("AVG")) {
-
-        }
+            for (int i = startingCell.getCol(); i <= lastCell.getCol(); i++) {
+                for (int j = startingCell.getRow(); j <= lastCell.getRow(); j++) {
+                    //something malicious is brewing
+                    int boals = i + (char)'A';
+                    char newColLetter = (char) boals;
+                    String newRowNum = j + 1 + "";
+                    SpreadsheetLocation selectedCell = new SpreadsheetLocation(newColLetter + newRowNum);
+                    // SpreadsheetLocation selectedCell = new SpreadsheetLocation(startingCell.getCol() + ((char)'A' + i) + startingCell.getRow() + i + "");
+                    ans += ((RealCell) spread.getCell(selectedCell)).getDoubleValue();
+                    iteratedCellCount++;
+                    
+                }
+            }
+            if (formula[0].equalsIgnoreCase("AVG")) {
+                return ans / iteratedCellCount;
+            } else {
+                return ans;
+            }
+        } 
 
         for (int i = 0; i < formula.length; i++) {
             char operator = ' ';
